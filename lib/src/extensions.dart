55a7b249c59cc62b242cbe299fbad6968ffd9a76
Robert Virkus <Robert.Virkus@enough.de>
2023-10-09 16:14:38 +0200

    chore: improve code style

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -1,18 +1,19 @@
+import 'package:collection/collection.dart' show IterableExtension;
 import 'package:enough_icalendar/enough_icalendar.dart';
 import 'package:enough_mail/enough_mail.dart';
-import 'package:collection/collection.dart' show IterableExtension;
 
 import 'builder.dart';
 
 const String _icalFlagHeader = '\$ical';
 
+/// iCal extension methods for enough_mail [MailClient]
 extension ExtensionMailClient on MailClient {
-  /// Changes the [participantStatus] for the given[calandar], e.g. to accept or decline a meeting request, generates and sends the corresponding message.
+  /// Changes the [participantStatus] for the given[calendar], e.g. to accept or decline a meeting request, generates and sends the corresponding message.
   ///
   /// When you specify the [originatingMessage] a flag will tried be stored marking this message as replied with the given [particpantStatus]. Compare [ExtensionMimeMessage.calendarParticipationStaus] getter.
   /// Optionally specify a [comment], the [productId] that ends up in the generated [VCalendar] reply
   /// and the [icsFilename] that defaults to `reply.ics`.
-  Future<dynamic> sendCalendarReply(
+  Future<void> sendCalendarReply(
     VCalendar calendar,
     ParticipantStatus participantStatus, {
     MimeMessage? originatingMessage,
@@ -38,11 +39,17 @@ extension ExtensionMailClient on MailClient {
             [];
         final sequence = MessageSequence.fromMessage(originatingMessage);
         try {
-          await store(sequence, [flagName, MessageFlags.answered],
-              action: StoreAction.add);
+          await store(
+            sequence,
+            [flagName, MessageFlags.answered],
+            action: StoreAction.add,
+          );
           if (existingIcalFlags.isNotEmpty) {
-            await store(sequence, existingIcalFlags.toList(),
-                action: StoreAction.remove);
+            await store(
+              sequence,
+              existingIcalFlags.toList(),
+              action: StoreAction.remove,
+            );
           }
         } catch (e, s) {
           print('Unable to store flag $flagName: $e $s');
@@ -53,9 +60,13 @@ extension ExtensionMailClient on MailClient {
 
   /// Sends out the given [calendar] invite.
   ///
-  /// Optionally specify an explanation text in [plainTextPart] and specify the [icsFilename].
-  Future<dynamic> sendCalendarInvite(VCalendar calendar,
-      {String? plainTextPart, String icsFilename = 'invite.ics'}) async {
+  /// Optionally specify an explanation text in [plainTextPart] and
+  /// specify the [icsFilename].
+  Future<void> sendCalendarInvite(
+    VCalendar calendar, {
+    String? plainTextPart,
+    String icsFilename = 'invite.ics',
+  }) async {
     final messageBuilder = VMessageBuilder.prepareFromCalendar(
       calendar,
       from: account.fromAddress,
@@ -66,8 +77,9 @@ extension ExtensionMailClient on MailClient {
   }
 }
 
+/// iCal extension methods for enough_mail [MimeMessage]
 extension ExtensionMimeMessage on MimeMessage {
-  /// Retriees the participant status from the flags of this message
+  /// Retries the participant status from the flags of this message
   ParticipantStatus? get calendarParticipantStatus {
     final flag =
         flags?.firstWhereOrNull((flag) => flag.startsWith(_icalFlagHeader));
@@ -76,7 +88,7 @@ extension ExtensionMimeMessage on MimeMessage {
         return ParticipantStatusParameter.parse(
             flag.substring(_icalFlagHeader.length));
       } catch (e) {
-        print('Warning: unknown iCal ParticipatonStatus flag found: [$flag]');
+        print('Warning: unknown iCal ParticipationStatus flag found: [$flag]');
       }
     }
     return null;
@@ -84,13 +96,15 @@ extension ExtensionMimeMessage on MimeMessage {
 }
 
 extension ExtensionCalendar on VCalendar {
-  /// Retrieves the attendee mailing addresses from the first component with a attendees getter.
+  /// Retrieves the attendee mailing addresses from the first component
+  /// with a attendees getter.
   List<MailAddress>? get attendeeMailAddresses => attendees
       ?.where((attendee) => attendee.email != null)
       .map((attendee) => MailAddress(attendee.commonName, attendee.email!))
       .toList();
 
-  /// Gets the organizer as a [MailAddress] from the first component with an organizer getter.
+  /// Gets the organizer as a [MailAddress] from the first component with
+  /// an organizer getter.
   MailAddress? get organizerMailAddress {
     final o = organizer;
     if (o == null || o.email == null) {
